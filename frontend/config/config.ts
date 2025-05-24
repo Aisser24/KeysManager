635761cd568fcb2747168c70640168c906364a44
Aisser24 <jonathanguger@gmail.com>
2025-05-24 15:28:53 +0200

    fix api fetch issue (fetch from server)

--- frontend/config/config.ts
+++ frontend/config/config.ts
@@ -1,5 +1,7 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const API_URL_BACKEND = process.env.NEXT_PUBLIC_API_URL_BACKEND || 'http://api/api';
 
 export const config = {
     apiUrl: API_URL,
+    apiUrlBackend: API_URL_BACKEND,
 }
